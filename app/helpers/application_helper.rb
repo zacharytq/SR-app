15eef737b0ac29b1253bde26486bdd7e8831d2a9
zacharytq <zacharytq@gmail.com>
2020-10-21 10:47:47 -0400

    more associations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def clearances
+        ["declassified", "classified", "secret", "top secret"]
+    end
+
+    def set_clearance
+        self.security_clearance = "declassified"
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
     has_many :events, through: :user_events
     has_many :notes
     has_many :documents
+    before_create :set_clearance
+
+    private
+    def set_clearance
+        self.security_clearance = "declassified"
+    end
 end
